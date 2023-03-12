7e6fd10bd568873245b19ba6dac724db09f9efe3
Jacyking <43704572+Jacyking@users.noreply.github.com>
2023-03-12 22:56:18 +0800

    optimize project

---
+++ cmake/build.cmake
@@ -0,0 +1,19 @@
+# Compile Standard
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+message(STATUS "CXX Standard: ${CMAKE_CXX_STANDARD}")
+
+# Build Type
+if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE "Release")
+endif()
+message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
+
+# libc++ or libstdc++&clang
+option(BUILD_WITH_LIBCXX "Build with libc++" OFF)
+if(BUILD_WITH_LIBCXX AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+    message(STATUS "Build with libc++")
+else()
+    message(STATUS "Build with libstdc++")
+endif()
