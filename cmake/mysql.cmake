a0571b7aedfb762285d0fd98bd3d45a8af17a460
Jacyking <791026912@qq.com>
2023-09-08 11:40:20 +0800

    test

--- cmake/mysql.cmake
+++ cmake/mysql.cmake
@@ -17,7 +17,7 @@ IF (WIN32)
   )
 ELSE (WIN32)
   FIND_PATH(MYSQL_INCLUDE_DIR mysql.h
-    /usr/local/opt/mysql*/include
+    /usr/local/opt/mysql@8.0/include
     /usr/local/include/mysql
     /usr/include/mysql
   )
@@ -36,7 +36,7 @@ ELSE (WIN32)
     NAMES ${MYSQL_NAMES}
     PATHS /usr/lib 
     /usr/local/lib
-    /usr/local/opt/mysql*/lib
+    /usr/local/opt/mysql@8.0/lib
     PATH_SUFFIXES mysql
   )
 ENDIF(WIN32)
