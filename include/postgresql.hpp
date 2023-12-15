9e4ec7c4cf9d56b7c51c0f9aaec898f1972d1b7a
Jacyking <791026912@qq.com>
2023-12-15 10:13:38 +0800

    support for enum

--- include/postgresql.hpp
+++ include/postgresql.hpp
@@ -520,22 +520,22 @@ class postgresql {
       }
     }
     else if constexpr (std::is_enum_v<U> && !iguana::is_int64_v<U>) {
-      char temp[20] = {};
+      std::vector<char> temp(20, 0);
       itoa_fwd(static_cast<int>(value), temp.data());
       param_values.push_back(std::move(temp));
     }
     else if constexpr (std::is_integral_v<U> && !iguana::is_int64_v<U>) {
-      char temp[20] = {};
+      std::vector<char> temp(20, 0);
       itoa_fwd(value, temp.data());
       param_values.push_back(std::move(temp));
     }
     else if constexpr (iguana::is_int64_v<U>) {
-      char temp[65] = {};
+      std::vector<char> temp(65, 0);
       xtoa(value, temp.data(), 10, std::is_signed_v<U>);
       param_values.push_back(std::move(temp));
     }
     else if constexpr (std::is_floating_point_v<U>) {
-      char temp[20] = {};
+      std::vector<char> temp(20, 0);
       sprintf(temp.data(), "%f", value);
       param_values.push_back(std::move(temp));
     }
